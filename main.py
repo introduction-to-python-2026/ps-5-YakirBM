7bd9db197cf08984fb950e654e03e214d78a522e
Yakir Ben Menashe-Personal <yakir.b.m.ite@gmail.com>
2025-11-20 21:32:17 +0200

    Clean up imports in main.py
    Removed unused import statements from equation_utils.py.

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-# Add the import statements for functions from string_utils.py and equation_utils.py here
 from string_utils import parse_chemical_reaction, count_atoms_in_reaction
 from equation_utils import build_equations, my_solve
-
-
-# equation_utils.py
-from sympy import Eq, symbols
-from sympy import solve as sympy_solve
 
 
 
